7002df58fc3115f5de8839a1485b7dc167aea593
kyphong2k <80156896+kyphong2k@users.noreply.github.com>
2022-07-10 22:00:20 +0700

    add emailjs to send email Successfully and auto reply

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,29 +1,44 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState} from 'react';
 import emailjs from '@emailjs/browser'
 import './Contact.scss';
 import myImage from '../../assets/images/myImage_contact.png'
 import Fade from 'react-reveal/Fade'
-
+import sendSuccessfully from '../../assets/images/SendSuccessfully.png'
 function Contact() {
   const form = useRef();
-
+  const [isFormShow, setIsFormShow] = useState(true)
   const sendEmail = e => {
     e.preventDefault();
-   
+    setIsFormShow(false)
+    emailjs.sendForm('service_gt8qote', 'template_olm4l17', form.current, 'DY3nkFG1qzzCYumV6')
+      .then(result => {
+        console.log(result.text)
+      }), (error) =>{
+        console.log(error)
+      }
+    console.log(form.current)
   }
   return (
       <div className='contact-wrapper'>
         <div className="contact-container">
           <Fade bottom duration={1150}>
             <div className="contact-form">
-              <form ref={form} onSubmit={sendEmail}>
-               <div id='form-info'>
-                 <input type="text" name='user_name' placeholder='Name'/>
-                 <input type='email' name='user_email' placeholder='Email'/>
-               </div>
-                <textarea id='text-message' rows="10" name='message' placeholder='Message to me'/>
+            { isFormShow && <form ref={form} onSubmit={sendEmail} >
+                <div id='form-info'>
+                  <input type="text" name='user_name' placeholder='Name' required/>
+                  <input type='email' name='user_email' placeholder='Email' required/>
+                </div>
+                {/* <input type='text' name='title' placeholder='Title'/> */}
+                <textarea id='text-message' rows="10" name='message' required placeholder='Message to me'/>
                 <input type='submit' value='Send Message'/>
-              </form>
+              </form> }
+              { !isFormShow && 
+                <div>
+                  <div className='form-sent'><img src={sendSuccessfully} alt='send success'/></div>
+                  <span id='return-form' onClick={() => setIsFormShow(true)}>Send another message</span>
+                </div>
+              
+              }
             </div>
           </Fade>
 
