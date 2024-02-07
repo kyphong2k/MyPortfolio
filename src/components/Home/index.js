bd3845ebcb100760662fd2b19e7433bfe3c6a1b3
phonghk3 <phonghk3@fpt.com>
2024-02-07 15:26:38 +0700

    Feat: add download Cv

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -6,7 +6,9 @@ import PanToolIcon from '@mui/icons-material/PanTool';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import myVideo from '../../assets/video/myHeroVideo.mp4';
+import cvPdf from '../../assets/pdf/HoangKyPhong-FrontEndWebDeveloper.pdf'
 import Fade from 'react-reveal/Fade'
+
 const Home = React.forwardRef((props,ref) => {
  
   return (
@@ -26,7 +28,10 @@ const Home = React.forwardRef((props,ref) => {
                         </Typed>
                     </h1>
                     <p>I have been working as a front-end web developer at an outsourcing company from January 2023 to the present and I am currently seeking a new, more challenging environment to further my career path.</p>
-                    <a className= 'contact-button' target='_blank' href='mailto:hoangkyphong926@gmail.com'><span>Contact Me</span></a>
+                    <div style={{display: 'flex', gap: '8px'}}>
+                      <a className= 'contact-button' target='_blank' href='mailto:hoangkyphong926@gmail.com'><span>Contact Me</span></a>
+                      <a className= 'contact-button' download target='_blank' href={cvPdf}><span>Download CV</span></a>
+                    </div>
                     <div className='social-media'>
                           <a href="https://www.facebook.com/profile.php?id=100014541090043" target="_blank" rel="noopener noreferrer">
                             <FacebookIcon sx={{ "&:hover": { color: "#3B5998" } }}  fontSize='32px'/>
